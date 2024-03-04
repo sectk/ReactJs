1d7a74957588862f2fc22cb10110bbfc69aa5f31
EnditaDwi <129390061+sectk@users.noreply.github.com>
2024-03-04 14:05:18 +0700

    Create nav.php

---
+++ nav.php
@@ -0,0 +1,13 @@
+function Nav(){
+    return (
+      <div className="App">
+       <ul>
+        <li>HOME</li>
+        <li>SEJARAH</li>
+        <li>TENTANG</li>
+        <li>Kontak</li>
+       </ul>
+      </div>
+    );
+  }
+  export default Nav;

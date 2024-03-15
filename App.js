b3713e54aa84b3a40a195b5fd9da8a98d664d96e
EnditaDwi <129390061+sectk@users.noreply.github.com>
2024-03-15 10:25:13 +0700

    Update App.js

--- App.js
+++ App.js
@@ -5,15 +5,20 @@ import Nav from './pages/Nav';
 import Sejarah from './pages/Sejarah';
 import Tentang from './pages/Tentang';
 import Home from './pages/Home';
+import {BrowserRouter, Router, Switch } from "react-router-dom";
 
 function App(){
     return (
-        <div className="App">
-        <Nav />
-        <Home />
-        <Kontak />
-        <Sejarah />
-        <Tentang />
-        </div>
-    )
+        <BrowserRouter>
+            <div className="App">
+                <Nav />
+                <Switch>
+                    <Router path='/' component={Home} exact />
+                    <Router path='/kontak' component={Kontak} />
+                    <Router path='/sejarah' component={Sejarah} />
+                    <Router path='/tentang' component={Tentang} />
+                </Switch>
+            </div>
+        </BrowserRouter> 
+    );
 }

cb1099abc1c0d515121dee8deba5f5df8ecfe75c
EnditaDwi <129390061+sectk@users.noreply.github.com>
2024-03-15 10:45:36 +0700

    Update and rename kontak.js to Kontak.js

---
+++ Kontak.js
@@ -0,0 +1,17 @@
+const css = {
+    color:"red",
+    background:"yelow",
+    fontSize:50
+}
+
+function Kontak(){
+    return (
+      <div className="App">
+        <h1 className="judul">kontak smkrevit</h1>
+        <p style={css}>
+            sidoarjo
+        </p>
+      </div>
+    );
+  }
+  export default Kontak;
--- kontak.js
+++
@@ -1,9 +0,0 @@
-function kontak(){
-    return (
-      <div className="App">
-        <h1>kontak smkrevit</h1>
-        <p>sidoarjo</p>
-      </div>
-    );
-  }
-  export default kontak;

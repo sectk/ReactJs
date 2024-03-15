85d621ff2a419677034ef4e7bb049802311e6b0c
EnditaDwi <129390061+sectk@users.noreply.github.com>
2024-03-15 10:26:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './app';
-import Kontak from './pages/kontak';
-import Nav from './pages/nav';
-import Sejarah from './pages/sejarah';
-import reportWebVitals from 'reportWebVitals';
+import App from './App';
+
+
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
-        <Kontak />
-    </React.StrictMode>
+      <App />
+    </React.StrictMode>,
     document.getElementById('root')
 );
 
-
-
 reportWebVitals();
-

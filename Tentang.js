cc2ce20db6d8e193d4ccbe2c2877e39146e3bb1d
EnditaDwi <129390061+sectk@users.noreply.github.com>
2024-03-15 10:44:57 +0700

    Update Tentang.js

--- Tentang.js
+++ Tentang.js
@@ -3,6 +3,8 @@ function Tentang(){
       <div className="App">
         <h1>tentang smkrevit</h1>
         <p>isi tentang smk revit</p>
+        <button type="button" class="btn btn-primary">ini tentang SmkRevit</button>
+        <button type="button" class="btn btn-success">ini tentang SmkRevit</button>
       </div>
     );
   }

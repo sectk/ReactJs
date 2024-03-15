764e757210fba1064ba51b265d08d9efa1c2156e
EnditaDwi <129390061+sectk@users.noreply.github.com>
2024-03-15 10:25:57 +0700

    Update and rename sejarah.php to Sejarah.js

--- Sejarah.js
+++ Sejarah.js
@@ -3,7 +3,9 @@ function Sejarah(){
       <div className="App">
         <h1>sejarah smkrevit</h1>
         <p>sidoarjo</p>
+        <img src="images/logo.png" alt="logo" srcset="" />
       </div>
     );
   }
-  export default Sejarah;
+
+ export default Sejarah;

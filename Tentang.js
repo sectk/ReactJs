7546c48880726a1d038defc0318cd8e8addd14d4
EnditaDwi <129390061+sectk@users.noreply.github.com>
2024-03-15 09:43:06 +0700

    Create Tentang.js

---
+++ Tentang.js
@@ -0,0 +1,10 @@
+function Tentang(){
+    return (
+      <div className="App">
+        <h1>tentang smkrevit</h1>
+        <p>isi tentang smk revit</p>
+      </div>
+    );
+  }
+
+ export default Tentang;
